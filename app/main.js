356f1721428e9eb0652e83c1678b33abe7ed2faa
VeselAbaya <Vandervise465@gmail.com>
2019-01-29 12:03:11 +0300

    bigger window size

--- app/main.js
+++ app/main.js
@@ -5,7 +5,7 @@ const {app, BrowserWindow, ipcMain} = electron
 let mainWindow
 let token
 app.on('ready', () => {
-    mainWindow = new BrowserWindow
+    mainWindow = new BrowserWindow({ width: 1200, height: 900 })
     mainWindow.loadURL(`file://${__dirname}/auth-form/auth-form.html`)
     // mainWindow.loadURL(`file://${__dirname}/ad-platforms-selector/ad-platforms-selector.html`)
 
