c0f4b48b42a7f5e42b302d19513491eed879403a
VeselAbaya <Vandervise465@gmail.com>
2019-02-15 13:36:12 +0300

    #

--- app/ad-form/ad-form.js
+++ app/ad-form/ad-form.js
@@ -5,16 +5,10 @@ import {ApiRequest} from "../common/apiRequest/ApiRequest";
 document.addEventListener('DOMContentLoaded', () => {
     const apiRequest = new ApiRequest('form')
     apiRequest.on('success', (res) => {
-        console.log(res)
         const formMarkup = res.data.form
-
         document.querySelector('.ad-form').insertAdjacentHTML('beforeend', formMarkup)
 
         const previewList = new PreviewList()
         selectsInit()
-    })
-
-    apiRequest.on('error', (err) => {
-        console.log(err)
     })
 })
